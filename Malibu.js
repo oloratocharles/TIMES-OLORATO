2dc8904d3eb21f5f240c8f8a6f8bd3ae2f64f948
oloratocharles <31730619+oloratocharles@users.noreply.github.com>
2021-06-30 17:00:24 +0200

    Delete Malibu.js

--- Malibu.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Home = (props) => (
-  <Wrapper>
-    <p>Originally published:May 25, 2021</p>
-    <p>Author: Taylor Jenkins Reid</p>
-    <p>Genre:fiction</p>
-  </Wrapper>
-)
